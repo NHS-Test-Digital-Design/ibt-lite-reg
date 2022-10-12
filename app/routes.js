1cd76e63fd3a963a61313055cabcea8f6e7a653f
Russell Poulter <russell.poulter@infinityworks.com>
2022-10-12 17:01:49 +0100

    added barcade, login choice and some ukhsa study pages and some routes

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,15 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+// registering-for.html routing.
+router.post('/registering-for-answer', function (req, res) {
+    var whoFor = req.session.data['registering-for']
+    if (whoFor == "someone-else"){
+        res.redirect('register-kit/barcode')
+    } else {
+        res.redirect('register-kit/login-choice')
+    }
+})
+
+
 module.exports = router
