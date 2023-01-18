41406b707e9321d6b95a7e748421df01284c5bcb
Arpana18 <91430010+Arpana18@users.noreply.github.com>
2023-01-18 18:58:31 +0000

    refactoring updates
    changes made to screen

--- app/routes.js
+++ app/routes.js
@@ -7,11 +7,33 @@ const router = express.Router()
 router.post('/registering-for-answer', function (req, res) {
     var whoFor = req.session.data['registering-for']
     if (whoFor == "someone-else"){
-        res.redirect('register-kit/barcode')
+        res.redirect('register-kit/login-choice')
     } else {
         res.redirect('register-kit/login-choice')
     }
 })
+
+
+// travel-overseas.html routing.
+router.post('/travel-answer', function (req, res) {
+    var travelFor = req.session.data['travel-overseas']
+    if (travelFor == "no"){
+        res.redirect('register-kit/test-location')
+    } else {
+        res.redirect('register-kit/travel-country')
+    }
+})
+
+// travel-location.html routing.
+router.post('/test-location-answer', function (req, res) {
+    var travelFor = req.session.data['test-location']
+    if (travelFor == "no"){
+        res.redirect('register-kit/test-site-code')
+    } else {
+        res.redirect('register-kit/test-site-code')
+    }
+})
+
 
 
 module.exports = router
